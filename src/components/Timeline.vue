77e98fabdefcaa9cc1f14e1b1b2f7dca4f18249d
Lei Chen <mashirocl@gmail.com>
2024-05-24 17:18:21 +0200

    feat: add sha1 for timeline

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,7 +1,9 @@
 <template>
+    <h3>Timeline</h3>
     <div class="timeline">
       <div v-for="(commit, index) in commits" :key="index" class="commit">
-        <div class="commit-circle" @click="commitClicked(commit)" :title="commit.message"></div>
+        <div class="commit-circle" @click="commitClicked(commit)" :title="commit[0].sha1"></div>
+        <div>{{commit[0].sha1.slice(0,7)}}</div>
       </div>
     </div>
   </template>
@@ -25,6 +27,8 @@
     display: flex;
     justify-content: space-between;
     align-items: center;
+    gap: 20px;
+    overflow-x: auto;
     height: 50px;
     background-color: #f0f0f0;
     border: 1px solid #ccc;
@@ -40,7 +44,12 @@
     background-color: #007bff;
     border-radius: 50%;
     cursor: pointer;
+    margin-left: 30px;
   }
+  .container div:last-child {
+  font-family: monospace; /* Monospaced font for sha1 */
+  font-size: 16px; /* Adjust font size as needed */
+}
   .commit-circle:hover {
     background-color: #0056b3;
   }
